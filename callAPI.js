46ff54ad6d15a9d2f88ab4048899ee5ff192a51a
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2022-03-10 10:04:51 +0700

    Call openweatherAPI

--- callAPI.js
+++ callAPI.js
@@ -1,12 +1,14 @@
 let request = require('request');
 require('dotenv').config();
-let url = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${process.env.WEATHER_API_KEY}`
+let lat = process.env.LATITUDE;
+let lon = process.env.LONGITUDE;
+let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${process.env.WEATHER_API_KEY}`
 
-request(url, function (err, response, body) {
+request(url, function (err, _, body) {
     if(err){
         console.log('error:', error);
     } else {
         let weather = JSON.parse(body);
         console.log(weather);
     }
-  });
+});
